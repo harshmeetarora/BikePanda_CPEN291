056c7f0f922bf5cba626beae32980d8ec946030b
Omar Agha <o.agha@alumni.ubc.ca>
2017-04-03 13:28:41 -0700

    Added getdatefromdb.php comments

--- Website/getdatefromdb.php
+++ Website/getdatefromdb.php
@@ -1,8 +1,8 @@
 <?php
 
-	/**
+	/*
 	 * Helper function to rename array keys. 
-	 * http://stackoverflow.com/questions/240660/in-php-how-do-you-change-the-key-of-an-array-element
+	 * from http://stackoverflow.com/questions/240660/in-php-how-do-you-change-the-key-of-an-array-element
 	 */
 	function _rename_arr_key($oldkey, $newkey, array &$arr) {
 	  if (array_key_exists($oldkey, $arr)) {
@@ -24,11 +24,10 @@
 
 		# split date into separate parts
 		$day = substr($date, 8, 9);
-
 		$month = $monthsVals[intval(substr($date, 5, 6))]; 
-
 		$year = substr($date, 0, 4);
 
+		# return formatted dated string
 		return ($month . "_" . $day . "_" . $year);
 	}
 
@@ -40,68 +39,74 @@
 	/* check connection */
 	if (mysqli_connect_errno())
 	{
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
+	    printf("Connect failed: %s\n", mysqli_connect_error());
+	    exit();
 	}
 
-	# array to hold 4 date values
-	$fiveDateValues = array();
-	//$dateVals = array_values($fiveDateValues);
+	# array to hold date values
+	$dateValues = array();
 
+	# read up to 500 date elements from the database
 	for ($i=0; $i < 500; $i++) 
 	{ 
+		# check if there's a valid result
 		if ($result = $mysqli->query("SELECT trip_number, DATE(time) as time FROM bikedata ORDER BY rowid DESC LIMIT 500 OFFSET $i"))
 		{
+			# fetch one row
 			$row = $result->fetch_assoc();
 
+			# separate trip number and datetime object
 			$trip_number = intval($row['trip_number']);
 			$latestdate = $row['time'];
 
+			# parse the datetime object to our format
 			$finaldate = parseTimeStamp($latestdate);
 
-			if (!in_array($finaldate, array_keys($fiveDateValues)))
+			# add date and trip to the dates array if it's not already there
+			if (!in_array($finaldate, array_keys($dateValues)))
 			{
+				# initialie the json array for that date
 				$trips = array('trip1' => $trip_number);
 
-				$fiveDateValues[$finaldate] = $trips; 
+				# add the json array template to the dates array
+				$dateValues[$finaldate] = $trips; 
 			} 
+			# do this if date was already initialized in the array. Add another trip
 			else
 			{
-				if (!in_array($trip_number, $fiveDateValues[$finaldate]))//trip is not in trip array
+				
+				# trip is not in trip array
+				if (!in_array($trip_number, $dateValues[$finaldate]))
 				{
-					array_push($fiveDateValues[$finaldate], $trip_number);
-
-					$keysOfDates = array_keys($fiveDateValues[$finaldate]);
+					# push a new trip into the json array
+					array_push($dateValues[$finaldate], $trip_number);
+					
+					
+					$keysOfDates = array_keys($dateValues[$finaldate]);
 
 					$lastArrElem = $keysOfDates[sizeof($keysOfDates)-1];
 
 					$beforeChange = $keysOfDates[sizeof($keysOfDates)-2];
 
+					# placeholder for the trip 
 					$tripPlaceholder = (string) intval(substr($beforeChange, 4)) + 1;
 
 					$consecutiveVal = "trip".$tripPlaceholder;
 
-					_rename_arr_key($lastArrElem, $consecutiveVal, $fiveDateValues[$finaldate]);
+					_rename_arr_key($lastArrElem, $consecutiveVal, $dateValues[$finaldate]);
 				} 
-				else
-				{
-
-				}			
-
-				
-
 			}
 			$result->close();
 		}
+		# connection error signal
 		else 
 		{
 	    	echo "Error: " . $sql . "<br>" . $conn->error;
 		}
 	}
-	//echo $dateVals;	
+	# encode the array into json
+	echo json_encode($dateValues);
 
-	//$finalDatesArr = array('arr' => $dateVals);
-	echo json_encode($fiveDateValues);
-
+	# close mysql connection
 	$mysqli->close();
 ?>
